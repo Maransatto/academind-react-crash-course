dc0ce9ae647e7c9673fe7c7ccf70cb6c91436351
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-10-31 08:38:45 -0300

    feat: :sparkles: 594 handling form submission

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,36 +1,19 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Miguel" body="Hey dad!" />
       </ul>
     </>
