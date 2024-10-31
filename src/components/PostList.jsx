357fe00284090f9a849be720ac4883cf4314c9b3
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-10-31 08:46:18 -0300

    feat: :sparkles: 595 updating state based on previous state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,13 +3,22 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import Modal from "./Modal";
+import { useState } from "react";
 
 function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((prevPosts) => {
+      return [postData, ...prevPosts];
+    });
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
 
