b9265d9a8d4c4fc82af58d2abb3c445a228a95fc
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-10-31 21:59:04 -0300

    feat: :sparkles: 605 linking & navigating

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import classes from "./NewPost.module.css";
+import { Link } from "react-router-dom";
 import Modal from "../components/Modal";
+import classes from "./NewPost.module.css";
 
-function NewPost({ onCancel, onAddPost }) {
+function NewPost({ onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -22,7 +23,6 @@ function NewPost({ onCancel, onAddPost }) {
       author: enteredAuthor,
     };
     onAddPost(postData);
-    onCancel();
   }
 
   return (
@@ -42,9 +42,9 @@ function NewPost({ onCancel, onAddPost }) {
           />
         </p>
         <p className={classes.actions}>
-          <button type="button" onClick={onCancel}>
+          <Link to=".." type="button">
             Cancel
-          </button>
+          </Link>
           <button>Submit</button>
         </p>
       </form>
